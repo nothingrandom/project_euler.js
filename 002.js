e5258f5316be0e553e48fad2eb6f82eacad12a16
Benjamin Hollway <benjamin@nothingrandom.com>
2014-04-22 22:54:21 +0100

    Add answer 002

---
+++ 002.js
@@ -0,0 +1,24 @@
+function fib(total, x) {
+  if(x === 0) {
+    return 1;
+  }
+  if(x === 1) {
+    return 2;
+  }
+  return total[x - 1] + total[x - 2];
+}
+
+var answer = 0;
+var total = [];
+
+for (var i = 0; i < 4000000; i++) {
+	total[i] = fib(total,i);
+  if(total[i] > 4000000) {
+    break;
+  }
+  if(total[i] % 2 === 0) {
+    answer += total[i];
+  }
+}
+
+console.log(answer);

25c5f516ad0afdfb9ec89bb81c3983c9126d71b5
Benjamin Hollway <benjamin@nothingrandom.com>
2014-04-22 23:31:54 +0100

    Add answer 003

---
+++ 003.js
@@ -0,0 +1,15 @@
+function highprime(x) {
+	var max = Math.round(Math.sqrt(x));
+
+	for(var i = max; i >= 2; i--) {
+
+		if(x % i === 0 && highprime(i) === 1) {
+			return i;
+		}
+
+	}
+
+	return 1;
+}
+
+console.log(highprime(600851475143));

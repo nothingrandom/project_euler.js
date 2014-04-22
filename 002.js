056a1a166b046f3eafa4613a6ad9558319d55bb4
Benjamin Hollway <benjamin@nothingrandom.com>
2014-04-22 23:28:11 +0100

    Make code clearer
    Fixed indentations/spaces and line breaks.

--- 002.js
+++ 002.js
@@ -1,11 +1,14 @@
 function fib(total, x) {
-  if(x === 0) {
-    return 1;
-  }
-  if(x === 1) {
-    return 2;
-  }
-  return total[x - 1] + total[x - 2];
+
+	if(x === 0) {
+		return 1;
+	}
+
+	if(x === 1) {
+		return 2;
+	}
+
+	return total[x - 1] + total[x - 2];
 }
 
 var answer = 0;
@@ -13,12 +16,15 @@ var total = [];
 
 for (var i = 0; i < 4000000; i++) {
 	total[i] = fib(total,i);
-  if(total[i] > 4000000) {
-    break;
-  }
-  if(total[i] % 2 === 0) {
-    answer += total[i];
-  }
+
+	if(total[i] > 4000000) {
+		break;
+	}
+
+	if(total[i] % 2 === 0) {
+		answer += total[i];
+	}
+
 }
 
 console.log(answer);

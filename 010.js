5c95644760fb0e6d9006813ef7be21fe789c6693
Benjamin Hollway <benjamin@nothingrandom.com>
2014-05-14 20:23:35 +0100

    Add answer 010

---
+++ 010.js
@@ -0,0 +1,18 @@
+var answer = 2;
+var mil = 2000000;
+
+for (var i = 2; i < mil; i++) {
+	if (i % 2 != 1) {
+		continue;
+	}
+	var a = 3;
+	var b = Math.sqrt(i);
+	while ((i % a !== 0) && a < b) {
+		a += 2;
+	}
+	if (((i % a === 0 && i != a) * 1) === 0) {
+		answer += i;
+	}
+}
+
+console.log(answer);

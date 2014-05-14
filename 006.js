642943535252f7de5d12662c96ab6cd0768565ba
Benjamin Hollway <benjamin@nothingrandom.com>
2014-05-14 11:34:24 +0100

    Add answer 006

---
+++ 006.js
@@ -0,0 +1,18 @@
+var square = 0;
+var sum = 0;
+var answer = 0;
+var number2 = 0;
+
+for (var i = 0; i <= 100; i++) {
+	var number = Math.pow(i,2);
+	square += number;
+}
+
+for(var i = 0; i <= 100; i++) {
+	number2 += i;
+	sum = Math.pow(number2,2);
+}
+
+var answer = sum - square;
+
+console.log(answer);

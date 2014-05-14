2192e400f804aaff883264b1f5d172d738523e52
Benjamin Hollway <benjamin@nothingrandom.com>
2014-05-14 20:10:12 +0100

    Add answer 009

---
+++ 009.js
@@ -0,0 +1,16 @@
+var answer = 0;
+
+loop:
+for (var i = 2; ; i++) {
+	for (var j = 1; j < i; j++) {
+		var a = (i * i) - (j * j);
+		var b = 2 * i * j;
+		var c = (i * i) + (j * j);
+		if (a + b + c == 1000) {
+			answer = a * b * c;
+			break loop;
+		}
+	}
+}
+
+console.log(answer);

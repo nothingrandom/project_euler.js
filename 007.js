cc9ce7a02fe60e8ecddd564d79164236b76358b0
Benjamin Hollway <benjamin@nothingrandom.com>
2014-05-14 17:01:10 +0100

    Add answer 007

---
+++ 007.js
@@ -0,0 +1,29 @@
+function isPrime(number) {
+	var prime = true;
+	var max = Math.round(Math.sqrt(number));
+	for (i = max; i >= 2; i--){
+		if(number % i === 0) {
+			prime = false;
+		}
+	}
+	return prime;
+}
+
+function primeCount(prime) {
+	var count = 1;
+	var answer = 0;
+	var i = 2;
+	while(count <= prime) {
+		if (isPrime(i) === true) {
+			answer = i;
+			count++;
+			i++;
+		}
+		else {
+			i++;
+		}
+	}
+	console.log(answer);
+}
+
+primeCount(10001);
